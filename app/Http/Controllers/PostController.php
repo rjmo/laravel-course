ca41550915d2f548a9587887e98fe3b9fb78d997
rjmo <radiorafael@hotmail.com>
2021-06-07 00:53:43 -0400

    error fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -17,9 +18,16 @@ class PostController extends Controller
     }
     public function store(Request $request)
     {
+
+
         $this->validate($request, [
             'body' => 'required',
         ]);
+
+        if (!Auth::check()) {
+            abort(401);
+        }
+        
         $request->user()->posts()->create([
             'body' => $request->body
         ]);
